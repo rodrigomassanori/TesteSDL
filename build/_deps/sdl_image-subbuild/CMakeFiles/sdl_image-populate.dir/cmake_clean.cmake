9bf01e85a7a43b227135f2045571db72c9456abb
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-09-21 15:43:38 -0300

    all

---
+++ build/_deps/sdl_image-subbuild/CMakeFiles/sdl_image-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl_image-populate"
+  "CMakeFiles/sdl_image-populate-complete"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-build"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-configure"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-download"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-install"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-mkdir"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-patch"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-test"
+  "sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sdl_image-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/tmp/sdl_image-populate-gitclone.cmake
@@ -0,0 +1,66 @@
+
+if(NOT "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitinfo.txt" IS_NEWER_THAN "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitclone-lastrun.txt")
+  message(STATUS "Avoiding repeated git clone, stamp file is up to date: 'C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitclone-lastrun.txt'")
+  return()
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E rm -rf "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-src"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to remove directory: 'C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-src'")
+endif()
+
+# try the clone 3 times in case there is an odd git clone issue
+set(error_code 1)
+set(number_of_tries 0)
+while(error_code AND number_of_tries LESS 3)
+  execute_process(
+    COMMAND "C:/Program Files/Git/cmd/git.exe"  clone --no-checkout --config "advice.detachedHead=false" "https://github.com/libsdl-org/SDL_image.git" "sdl_image-src"
+    WORKING_DIRECTORY "C:/Users/rodri/Documents/TesteSDL/build/_deps"
+    RESULT_VARIABLE error_code
+    )
+  math(EXPR number_of_tries "${number_of_tries} + 1")
+endwhile()
+if(number_of_tries GREATER 1)
+  message(STATUS "Had to git clone more than once:
+          ${number_of_tries} times.")
+endif()
+if(error_code)
+  message(FATAL_ERROR "Failed to clone repository: 'https://github.com/libsdl-org/SDL_image.git'")
+endif()
+
+execute_process(
+  COMMAND "C:/Program Files/Git/cmd/git.exe"  checkout release-2.6.2 --
+  WORKING_DIRECTORY "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-src"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to checkout tag: 'release-2.6.2'")
+endif()
+
+set(init_submodules TRUE)
+if(init_submodules)
+  execute_process(
+    COMMAND "C:/Program Files/Git/cmd/git.exe"  submodule update --recursive --init 
+    WORKING_DIRECTORY "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-src"
+    RESULT_VARIABLE error_code
+    )
+endif()
+if(error_code)
+  message(FATAL_ERROR "Failed to update submodules in: 'C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-src'")
+endif()
+
+# Complete success, update the script-last-run stamp file:
+#
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E copy
+    "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitinfo.txt"
+    "C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitclone-lastrun.txt"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR "Failed to copy script-last-run stamp file: 'C:/Users/rodri/Documents/TesteSDL/build/_deps/sdl_image-subbuild/sdl_image-populate-prefix/src/sdl_image-populate-stamp/sdl_image-populate-gitclone-lastrun.txt'")
+endif()
+
